c5d2ddec17aefea5585448e0254cb097c92dd6af
MoathOmar <moath@foothillsolutions.com>
2019-09-30 08:43:11 +0300

    New Post

---
+++ src/components/NewPost.tsx
@@ -0,0 +1,84 @@
+import * as React from "react";
+import { makeStyles } from "@material-ui/styles";
+import { Typography, Button } from "@material-ui/core";
+import { fontSize, colors } from "../SystemVariables";
+import { MyInput } from "./Input";
+// import MyInput from "./Input";
+
+const useStyle = makeStyles({
+  form: {
+    width: "500px",
+    "& div": {
+      textAlign: "center"
+    }
+  },
+  title: {
+    height: 50,
+    paddingLeft: 20,
+    fontSize: 18
+  },
+  input: {
+    width: 900,
+    margin: "auto",
+    marginTop: 16,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    "&::placeholder": {
+      color: "black"
+    }
+  },
+  button: {
+    textTransform: "none",
+    color: "white",
+    fontSize: fontSize.normal,
+    background: colors.PrimaryColor,
+    marginTop: 16,
+    height: 50,
+    width: 180,
+    marginLeft: 320,
+    "&:hover": {
+      background: colors.PrimaryDark
+    }
+  },
+  small: {
+    paddingLeft: 10,
+    fontSize: 14
+  },
+
+  multiline: {
+    height: "auto"
+  }
+});
+export const NewPost = () => {
+  const classes = useStyle();
+  return (
+    <form className={classes.form}>
+      <div>
+        <MyInput
+          className={classes.input + " " + classes.title}
+          placeholder="Artical Title"
+        />
+        <MyInput
+          className={classes.input}
+          placeholder="What's this artical about?"
+        />
+        <MyInput
+          multiline
+          rows="8"
+          className={
+            classes.input + " " + classes.multiline + " " + classes.small
+          }
+          placeholder="Write you're artical (in markdown)"
+        />
+        <MyInput
+          className={classes.input + " " + classes.small}
+          placeholder="Enter tags"
+        />
+      </div>
+      <Button className={classes.button + " " + classes.small} disableRipple>
+        Publish Artical
+      </Button>
+    </form>
+  );
+};
--- stories/containors.stories.tsx
+++ stories/containors.stories.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { SignUp, SignIn } from "../src/components/SignUpIn";
 import { Settings } from "../src/components/Settings";
+import { NewPost } from "../src/components/NewPost";
 
 storiesOf("settings", module).add("settings", () => <Settings />);
 storiesOf("SignUp", module).add("SignUp", () => <SignUp />);
 storiesOf("Signin", module).add("SignIn", () => <SignIn />);
+storiesOf("New Post", module).add("New Post", () => <NewPost />);
